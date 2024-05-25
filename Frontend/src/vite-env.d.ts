e463951e03a14c47418c7866efafa4ca504d2013
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-25 18:40:55 +0545

    add environment variables for development

--- Frontend/src/vite-env.d.ts
+++ Frontend/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+// types declaration for environment variables - vite
+interface ImportMetaEnv {
+    readonly VITE_BACKEND_ENDPOINT: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
