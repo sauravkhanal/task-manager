8729d2aecb9af45846f48adab2ec3c8ad7019e95
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-25 21:53:13 +0545

    set up cors

--- Backend/src/middleware/index.ts
+++ Backend/src/middleware/index.ts
@@ -1,14 +1,16 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import decodeAccessToken from "./decodeAccessToken"
+import express from "express";
+import cookieParser from "cookie-parser";
+import decodeAccessToken from "./decodeAccessToken";
+import cors from "cors";
 
-const middleware = express()
+const middleware = express();
 
-middleware.use(express.json())
-middleware.use(express.urlencoded({extended: false}))
-middleware.use(cookieParser())
-middleware.use(express.static("public"))
-middleware.use(decodeAccessToken)
+middleware.use(cors({ origin: "http://localhost:5173", credentials: true }));
+//if withCredentials: true in request, wild card cors entry "*" doesn't work.
+middleware.use(express.json());
+middleware.use(express.urlencoded({ extended: false }));
+middleware.use(cookieParser());
+middleware.use(express.static("public"));
+middleware.use(decodeAccessToken);
 
-
-export default middleware
+export default middleware;
