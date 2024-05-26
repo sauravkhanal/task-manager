bf26e59f288f74d6918f40264a111aa2deac7566
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-26 22:48:54 +0545

    change email message format

--- Backend/src/utils/mail/index.ts
+++ Backend/src/utils/mail/index.ts
@@ -1,4 +1,4 @@
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 import env from "../../config/env";
 
 const transporter = nodemailer.createTransport({
@@ -11,21 +11,37 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-
 export default async function sendMail(recipient: string, OTP: number) {
-
     const mailOptions = {
         from: env.email,
         to: recipient,
         subject: "TASK-MANAGER Verify your account.",
-        text: `Your OTP is : ${OTP} \n Enter this otp in the app. \n Go to ${env.endpoint}/v1/auth/verify/${OTP}`
+        text: `Hello,
+
+        Thank you for signing up for TASK-MANAGER!
+
+        To complete your registration, please verify your account using the One-Time Password (OTP) provided below:
+
+        Your OTP: ${OTP}
+
+        You can enter this OTP in the app or use the following links to verify your account:
+
+        http://localhost:5173/verify/${OTP}
+        or
+        ${env.endpoint}/v1/auth/verify/${OTP}
+
+        If you did not create an account with TASK-MANAGER, please disregard this email.
+
+        Best regards,
+        The TASK-MANAGER Team
+        `,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            console.error('❌ Error:', error.message);
+            console.error("❌ Error:", error.message);
         } else {
-            console.log('✅ Email sent:', info.response);
+            console.log("✅ Email sent:", info.response);
         }
     });
 }
