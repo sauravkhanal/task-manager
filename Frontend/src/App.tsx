34b41440e6cc588db90a30c597a2563c8a2cd639
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-26 14:14:55 +0545

    separate routes file and protect routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,26 +1,20 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeProvider";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import VerifyOTP from "./pages/VerifyOTP";
 import NavBar from "./components/NavBar";
+import AuthProvider from "./context/authContext";
+import { BrowserRouter } from "react-router-dom";
+import MyRoutes from "./components/Routes";
 
 function App() {
     return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <BrowserRouter>
-                <NavBar className="lg:px-20" />
-                <div className="lg:px-20 font-poppins">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/verify" element={<VerifyOTP />} />
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </ThemeProvider>
+        <AuthProvider>
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <BrowserRouter>
+                    <NavBar className="lg:px-20" />
+                    <div className="lg:px-20 font-poppins"></div>
+                    <MyRoutes />
+                </BrowserRouter>
+            </ThemeProvider>
+        </AuthProvider>
     );
 }
 
---
+++ Frontend/src/components/Routes/index.tsx
@@ -0,0 +1,43 @@
+import { Route, Routes, Outlet, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "@/context/authContext";
+import Home from "@/pages/Home";
+import VerifyOTP from "@/pages/VerifyOTP";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import Dashboard from "@/pages/dashboard";
+type TRouteProps = {
+    isLoggedIn: boolean;
+    redirectPath?: string;
+};
+const ProtectedRoute = ({ isLoggedIn, redirectPath = "/" }: TRouteProps) => {
+    if (!isLoggedIn) {
+        return <Navigate to={redirectPath} replace />;
+    }
+    return <Outlet />;
+};
+const PreventedRoute = ({
+    isLoggedIn,
+    redirectPath = "/dashboard",
+}: TRouteProps) => {
+    if (isLoggedIn) {
+        return <Navigate to={redirectPath} replace />;
+    }
+    return <Outlet />;
+};
+export default function MyRoutes() {
+    const { isLoggedIn } = useContext(AuthContext);
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/verify" element={<VerifyOTP />} />
+            <Route element={<PreventedRoute isLoggedIn={isLoggedIn} />}>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Route>
+            <Route element={<ProtectedRoute isLoggedIn={isLoggedIn} />}>
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+        </Routes>
+    );
+}
