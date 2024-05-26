29de143cb7467795b97162dd31a4f610185402e1
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-26 14:14:38 +0545

    add local storage access utility

---
+++ Frontend/src/utils/localStorage/index.ts
@@ -0,0 +1,20 @@
+import { ILoginResponse, IUserDetails } from "@/types";
+
+const local = {
+    save({ accessToken, refreshToken, userDetails }: ILoginResponse) {
+        localStorage.setItem("accessToken", accessToken);
+        localStorage.setItem("refreshToken", refreshToken);
+        localStorage.setItem("userDetails", JSON.stringify(userDetails));
+    },
+
+    get(key: keyof ILoginResponse): string | IUserDetails | null {
+        if (key === "userDetails") return JSON.parse(localStorage.getItem("userDetails") || "{}");
+        else return localStorage.getItem(key);
+    },
+
+    clear() {
+        localStorage.clear();
+    },
+};
+
+export default local;
