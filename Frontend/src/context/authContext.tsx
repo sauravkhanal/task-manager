e5df561d98b0ca915aaed5df6a302aa431342d86
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-26 14:13:28 +0545

    implement auth context

---
+++ Frontend/src/context/authContext.tsx
@@ -0,0 +1,56 @@
+import { createContext, useState } from "react";
+import { ILoginResponse, IUserDetails } from "@/types";
+import local from "@/utils/localStorage";
+
+type TAuthContext = {
+    userDetails: IUserDetails | null;
+    isLoggedIn: boolean;
+    getUserDetails: () => IUserDetails | null;
+    login: (userLoginData: ILoginResponse) => void;
+    logout: () => void;
+};
+
+export const AuthContext = createContext<TAuthContext>({
+    userDetails: null,
+    isLoggedIn: false,
+    getUserDetails: () => null,
+    login: () => null,
+    logout: () => {},
+});
+
+export default function AuthProvider({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const [userDetails, setUserDetails] = useState<IUserDetails | null>(
+        local.get("userDetails") as IUserDetails,
+    );
+
+    const login = (userLoginData: ILoginResponse) => {
+        local.save(userLoginData);
+        setUserDetails(local.get("userDetails") as IUserDetails);
+    };
+
+    const logout = () => {
+        setUserDetails(null);
+    };
+
+    const getUserDetails = () => {
+        return userDetails;
+    };
+
+    return (
+        <AuthContext.Provider
+            value={{
+                userDetails,
+                isLoggedIn: !!userDetails,
+                getUserDetails,
+                login,
+                logout,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+}
