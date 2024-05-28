1af6743de5791cd163faaa6df123b6d159fc5e5b
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-28 23:57:09 +0545

    add loading icon component

---
+++ Frontend/src/components/LoadingIcon/index.tsx
@@ -0,0 +1,42 @@
+import Loader from "react-spinners/SyncLoader";
+import { useTheme } from "next-themes";
+
+export default function LoadingIcon({
+    text,
+    isLoading,
+    className,
+    color,
+    size = 8,
+    ...restProps
+}: {
+    text: string;
+    isLoading: boolean;
+    className?: string;
+    color?: string;
+    size?: number;
+    [key: string]: any;
+}) {
+    const { theme } = useTheme();
+    const color1 = color ? color : theme == "dark" ? "#000000" : "#ffffff";
+    console.log(color1);
+    return (
+        <div
+            className={`flex justify-center items-center ${className}`}
+            {...restProps}
+        >
+            <Loader
+                loading={isLoading}
+                className="absolute"
+                color={color1}
+                size={size}
+            />
+            <span>
+                {isLoading ? (
+                    <p className="opacity-0">{text}</p>
+                ) : (
+                    <p>{text}</p>
+                )}
+            </span>
+        </div>
+    );
+}
