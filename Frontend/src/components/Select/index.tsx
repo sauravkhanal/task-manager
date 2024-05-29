244e5f23dd58322025d719cd0c5ad620f52d0464
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-29 10:32:50 +0545

    fix: priority not changing

--- Frontend/src/components/Select/index.tsx
+++ Frontend/src/components/Select/index.tsx
@@ -29,7 +29,7 @@ export function SelectItems({
 }) {
     useEffect(() => {
         setValue("priority", prevValue ?? TaskPriority.LOW);
-    });
+    }, []);
     return (
         <Select
             onValueChange={(value) => {
