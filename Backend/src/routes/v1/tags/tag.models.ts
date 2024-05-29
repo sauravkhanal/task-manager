8260721f044dead843666067b1ca3f2b57275540
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-29 11:58:00 +0545

    add timestamp

--- Backend/src/routes/v1/tags/tag.models.ts
+++ Backend/src/routes/v1/tags/tag.models.ts
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 import ITag from "./types";
 
-const tagSchema = new mongoose.Schema<ITag>({
-    authorID: { type: String, required: true },
-    taskIDs: { type: [String], default: [] },
-    title: { type: String, required: true, unique: true },
-    description: { type: String },
-    color: { type: String, required: true },
-    deleted: { type: Boolean, default: false },
-});
+const tagSchema = new mongoose.Schema<ITag>(
+    {
+        authorID: { type: String, required: true },
+        taskIDs: { type: [String], default: [] },
+        title: { type: String, required: true, unique: true },
+        description: { type: String },
+        color: { type: String, required: true },
+        deleted: { type: Boolean, default: false },
+    },
+    { timestamps: true },
+);
 
 const TagModel = mongoose.model<ITag>("Tag", tagSchema);
 export default TagModel;
--- Backend/src/routes/v1/tasks/task.model.ts
+++ Backend/src/routes/v1/tasks/task.model.ts
@@ -2,29 +2,32 @@ import mongoose from "mongoose";
 import ITask, { TaskPriority } from "./types";
 import { WorkflowStage } from "../workflowStage/types";
 
-const taskSchema = new mongoose.Schema<ITask>({
-    title: { type: String, required: true, unique: true },
-    description: { type: String },
-    tagIDs: { type: [String], required: true },
-    dueDate: { type: Date, required: true },
-    priority: {
-        type: String,
-        enum: Object.values(TaskPriority),
-        required: true,
+const taskSchema = new mongoose.Schema<ITask>(
+    {
+        title: { type: String, required: true, unique: true },
+        description: { type: String },
+        tagIDs: { type: [String], required: true },
+        dueDate: { type: Date, required: true },
+        priority: {
+            type: String,
+            enum: Object.values(TaskPriority),
+            required: true,
+        },
+        workflowStage: {
+            type: String,
+            enum: Object.values(WorkflowStage),
+            required: true,
+            default: WorkflowStage.TODO,
+        },
+        creatorID: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        assigneeIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }],
+        commentIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+        attachmentIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Attachment" }],
+        activityIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Activity" }],
+        deleted: { type: Boolean, default: false },
     },
-    workflowStage: {
-        type: String,
-        enum: Object.values(WorkflowStage),
-        required: true,
-        default: WorkflowStage.TODO,
-    },
-    creatorID: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    assigneeIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }],
-    commentIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
-    attachmentIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Attachment" }],
-    activityIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Activity" }],
-    deleted: { type: Boolean, default: false },
-});
+    { timestamps: true },
+);
 
 const TaskModel = mongoose.model<ITask>("Task", taskSchema);
 export default TaskModel;
