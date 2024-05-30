454daea1a3b534ed209f0b9ab5e028f219038ae0
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-31 01:39:32 +0545

    use select options to change workflow stage

--- Frontend/src/components/TaskTable/ChangeWorkflowStageDialog/index.tsx
+++ Frontend/src/components/TaskTable/ChangeWorkflowStageDialog/index.tsx
@@ -1,15 +1,6 @@
 import taskAPI from "@/api/taskAPI";
 import LoadingIcon from "@/components/LoadingIcon";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog";
 import {
     Select,
     SelectContent,
@@ -20,9 +11,8 @@ import {
 } from "@/components/ui/select";
 import useDataContext from "@/context/dataContext";
 import { IAllTask, WorkflowStage } from "@/types";
-import { workflowStages } from "@/utils/constants";
-import { MoveRight } from "lucide-react";
-import { FormEventHandler, useState } from "react";
+import { workflowStages as priorityData } from "@/utils/constants";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export function ChangeWorkflowStageDialog({
@@ -31,98 +21,59 @@ export function ChangeWorkflowStageDialog({
     taskDetail: IAllTask;
 }) {
     const dataContext = useDataContext();
-    const [selectedStage, setSelectedStage] = useState(
+    const [selectedPriority, setSelectedPriority] = useState<WorkflowStage>(
         taskDetail.workflowStage,
     );
-    const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+    // const [dialogOpen, setDialogOpen] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(false);
-    const handleStageChange = (value: WorkflowStage) => {
-        setSelectedStage(value);
-    };
 
-    const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+    const handlePriorityChange = async (value: WorkflowStage) => {
         setLoading(true);
-        event.preventDefault();
-        event.stopPropagation();
-        const response = await taskAPI.ChangeWorkflowStage({
+        const response = await taskAPI.updateTask({
             id: taskDetail._id,
-            currentWorkflowStage: taskDetail.workflowStage,
-            newWorkflowStage: selectedStage,
+            taskDetails: {
+                workflowStage: value,
+            },
         });
         if (response.success) {
             toast.success(response.message);
             dataContext.refreshData({ tasks: true });
-            setDialogOpen(false);
-        } else if (!response.success) {
+            setSelectedPriority(value);
+            // setDialogOpen(false);
+        } else {
             toast.error(response.message);
         }
         setLoading(false);
     };
 
     return (
-        <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-            <DialogTrigger asChild>
-                <span>
-                    <Badge
-                        variant={taskDetail.workflowStage}
-                        onClick={() => setDialogOpen(true)}
-                    >
-                        {taskDetail.workflowStage}
+        <Select
+            value={selectedPriority}
+            onValueChange={handlePriorityChange}
+            disabled={loading}
+            // defaultOpen
+        >
+            <SelectTrigger minimal={true}>
+                <span className="flex justify-center">
+                    <Badge variant={selectedPriority}>
+                        <LoadingIcon
+                            text={selectedPriority}
+                            isLoading={loading}
+                            color="#ffffff"
+                        />
                     </Badge>
                 </span>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                    <DialogTitle>Edit workflow Stage</DialogTitle>
-                    <DialogDescription>
-                        Make changes to workflow stage of the task here.
-                    </DialogDescription>
-                </DialogHeader>
-                <form
-                    className="flex flex-col gap-5 justify-center w-full"
-                    onSubmit={handleSubmit}
-                >
-                    <div className="flex items-center gap-5 justify-center py-5">
-                        <span className="flex gap-1 items-center">
-                            <Badge variant={taskDetail.workflowStage}>
-                                {taskDetail.workflowStage}
-                            </Badge>
-                        </span>
-                        <MoveRight />
-                        <Select
-                            value={selectedStage}
-                            onValueChange={handleStageChange}
-                        >
-                            <SelectTrigger className="w-36">
-                                <span className="flex justify-center">
-                                    <Badge variant={selectedStage}>
-                                        {selectedStage}
-                                    </Badge>
-                                </span>
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectGroup>
-                                    <SelectLabel>Select new stage</SelectLabel>
-                                    {workflowStages.map((item) => (
-                                        <SelectItem
-                                            value={item.title}
-                                            key={item.title}
-                                        >
-                                            <Badge variant={item.title}>
-                                                {item.title}
-                                            </Badge>
-                                        </SelectItem>
-                                    ))}
-                                </SelectGroup>
-                            </SelectContent>
-                        </Select>
-                    </div>
-
-                    <Button type="submit" className="grow">
-                        <LoadingIcon text="Save Changes" isLoading={loading} />
-                    </Button>
-                </form>
-            </DialogContent>
-        </Dialog>
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectLabel>Change Stage</SelectLabel>
+                    {priorityData.map((item) => (
+                        <SelectItem value={item.title} key={item.title}>
+                            <Badge variant={item.title}>{item.title}</Badge>
+                        </SelectItem>
+                    ))}
+                </SelectGroup>
+            </SelectContent>
+        </Select>
     );
 }
