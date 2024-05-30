754cc450905197eea1002329ee3a108a43360fe0
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-30 12:18:27 +0545

    add all files into folder

--- Frontend/src/components/TaskTable/ColumnVisibilityDropdown/ColumnVisibilityDropdown.tsx
+++ Frontend/src/components/TaskTable/ColumnVisibilityDropdown/ColumnVisibilityDropdown.tsx
@@ -1,14 +1,13 @@
-import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { ChevronDown } from "lucide-react";
+
+import { Table } from "@tanstack/react-table";
 import {
     DropdownMenu,
     DropdownMenuCheckboxItem,
     DropdownMenuContent,
     DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
-import { Table } from "@tanstack/react-table";
-import { Check } from "lucide-react";
+} from "@/components/ui/dropdown-menu";
 
 interface ColumnVisibilityDropdownProps {
     table: Table<any>;
@@ -23,10 +22,7 @@ const ColumnVisibilityDropdown: React.FC<ColumnVisibilityDropdownProps> = ({
                 Columns <ChevronDown className="ml-2 h-4 w-4" />
             </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent
-            align="end"
-            className="z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
-        >
+        <DropdownMenuContent align="end">
             {table
                 .getAllColumns()
                 .filter((column) => column.getCanHide())
@@ -34,15 +30,11 @@ const ColumnVisibilityDropdown: React.FC<ColumnVisibilityDropdownProps> = ({
                     return (
                         <DropdownMenuCheckboxItem
                             key={column.id}
-                            className="capitalize relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
                             checked={column.getIsVisible()}
                             onCheckedChange={(value) =>
                                 column.toggleVisibility(!!value)
                             }
                         >
-                            {column.getIsVisible() && (
-                                <Check size={12} className="absolute left-2" />
-                            )}
                             {column.id}
                         </DropdownMenuCheckboxItem>
                     );
