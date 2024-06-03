ba180d4a417d0052c2f3bb4af15605f5cdb396c2
sauravkhanal <sauravkhanal635@gmail.com>
2024-06-04 01:14:22 +0545

    add avatar export

--- Frontend/src/components/CreateTask/SelectUser/UserCard.tsx
+++ Frontend/src/components/CreateTask/SelectUser/UserCard.tsx
@@ -3,6 +3,59 @@ import { UserRoundMinus } from "lucide-react";
 import fullName from "@/utils/fullName";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { cn } from "@/lib/utils";
+
+export function UserAvatar({
+    profileUrl,
+    firstName,
+    lastName,
+    size = 5,
+    className,
+    hoverContentClassName,
+    ...props
+}: {
+    profileUrl: string;
+    firstName: string;
+    lastName: string;
+    size?: number;
+    className?: string;
+    hoverContentClassName?: string;
+}) {
+    return (
+        <HoverCard>
+            <HoverCardTrigger>
+                <Avatar
+                    className={cn(
+                        "cursor-pointer hover:scale-105 transition",
+                        className,
+                    )}
+                    {...props}
+                >
+                    <AvatarImage
+                        src={profileUrl}
+                        width={size}
+                        height={size}
+                        className="object-cover"
+                    />
+                    <AvatarFallback>
+                        {firstName.charAt(0)}
+                        {lastName.charAt(0)}
+                    </AvatarFallback>
+                </Avatar>
+            </HoverCardTrigger>
+            <HoverCardContent
+                className={cn("w-max text-sm p-3", hoverContentClassName)}
+            >
+                Created by : {firstName} {lastName}
+            </HoverCardContent>
+        </HoverCard>
+    );
+}
 
 export default function UserCard({
     profileUrl,
@@ -10,23 +63,24 @@ export default function UserCard({
     middleName,
     lastName,
     onRemove,
+    size = 5,
 }: {
     profileUrl: string;
     firstName: string;
     middleName?: string;
     lastName: string;
+    size?: number;
     onRemove: () => void;
 }) {
     return (
         <Card className="relative rounded-l-full rounded-r-full grow-0 group shadow-none">
             <div className="flex gap-2 h-10 items-center pr-2">
-                <Avatar>
-                    <AvatarImage src={profileUrl} width={5} height={5} />
-                    <AvatarFallback>
-                        {firstName.charAt(0)}
-                        {lastName.charAt(0)}
-                    </AvatarFallback>
-                </Avatar>
+                <UserAvatar
+                    profileUrl={profileUrl}
+                    firstName={firstName}
+                    lastName={lastName}
+                    size={size}
+                />
                 <p className="font-medium text-sm">
                     {fullName({ firstName, middleName, lastName })}
                 </p>
