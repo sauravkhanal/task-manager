f5520103e0bbed3b771d7a98c134aeba8665eff2
sauravkhanal <sauravkhanal635@gmail.com>
2024-06-06 23:57:33 +0545

    implement close on click on blank places

--- Frontend/src/context/modalContext.tsx
+++ Frontend/src/context/modalContext.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { X } from "lucide-react";
 import { createContext, useContext, useState } from "react";
 
 interface IModalContext {
@@ -34,16 +35,23 @@ export const ModalProvider: React.FC<{ children: React.ReactNode }> = ({
         <ModalContext.Provider value={{ showModal, hideModal }}>
             {children}
             {modalContent && (
-                <div className="absolute top-0 left-0 w-full min-h-screen backdrop-blur-md bg-black bg-opacity-5 flex justify-center items-center z-20 p-5 md:p-10 xl:p-20">
+                <div
+                    className="absolute top-0 left-0 w-full min-h-screen backdrop-blur-md bg-black bg-opacity-5 flex justify-center items-center z-20 p-5 md:p-10 xl:p-20"
+                    onClick={() => hideModal()}
+                >
                     <Button
                         onClick={hideModal}
+                        size="icon"
                         className="absolute top-5 right-5 z-30"
                         variant="destructive"
                         title="close"
                     >
-                        X
+                        <X />
                     </Button>
-                    <div className="flex flex-col justify-center items-center h-full w-full">
+                    <div
+                        className="flex flex-col justify-center items-center h-auto max-w-2xl min-w-[75vw] md:min-w-[50vw] hover:bg-"
+                        onClick={(e) => e.stopPropagation()}
+                    >
                         {modalContent}
                     </div>
                 </div>
