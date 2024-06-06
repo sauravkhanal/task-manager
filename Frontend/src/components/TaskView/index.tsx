c28c0b4240e63ed226d994e778395abe98485688
sauravkhanal <sauravkhanal635@gmail.com>
2024-06-07 00:36:10 +0545

    add update task , open task button

--- Frontend/src/components/TaskView/index.tsx
+++ Frontend/src/components/TaskView/index.tsx
@@ -13,6 +13,8 @@ import { Badge } from "../ui/badge";
 import { TaskDueDate } from "../TaskCard";
 import { Calendar, Tag, User } from "lucide-react";
 import { useModal } from "@/context/modalContext";
+import UpdateTaskButton from "../UpdateTaskButton";
+import OpenTaskInNewTabButton from "../OpenTaskInNewTabButton";
 
 export function TaskView({ task }: { task: ITaskWithDetails }) {
     const { hideModal } = useModal();
@@ -22,36 +24,44 @@ export function TaskView({ task }: { task: ITaskWithDetails }) {
                 <CardTitle className="capitalize text-xl flex items-center gap-2">
                     <Badge variant={task.priority}>{task.priority}</Badge>
                     {task.title}
-                    <Badge variant={task.workflowStage} className="ml-auto">
-                        {task.workflowStage}
-                    </Badge>
+
+                    <UpdateTaskButton
+                        taskDetails={task}
+                        variant={"outline"}
+                        className="ml-auto"
+                    />
+                    <OpenTaskInNewTabButton taskID={task._id} />
                 </CardTitle>
             </CardHeader>
             <CardContent className="flex flex-col gap-5 w-full justify-center">
                 {/* assignees */}
                 <Card className="rounded-sm">
-                    <CardHeader>
-                        <CardTitle className="flex items-center gap-1">
-                            <User className="size-5" />
-                            Assignees ({task.assignees.length})
-                        </CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        {task.assignees.length > 0 && (
-                            <div className="flex flex-row flex-wrap gap-2 p-1  rounded-sm max-h-28 overflow-y-auto items-center">
-                                {task.assignees.map((value) => (
-                                    <UserCard
-                                        key={value._id}
-                                        firstName={value.firstName}
-                                        middleName={value.middleName}
-                                        lastName={value.lastName}
-                                        profileUrl={value.profilePicture}
-                                        onRemove={undefined}
-                                    />
-                                ))}
-                            </div>
-                        )}
-                    </CardContent>
+                    {task.assignees.length > 0 ? (
+                        <>
+                            <CardHeader className="p-4">
+                                <CardTitle className="flex items-center gap-1">
+                                    <User className="size-5" />
+                                    Assignees ({task.assignees.length})
+                                </CardTitle>
+                            </CardHeader>
+                            <CardContent className="p-4">
+                                <div className="flex flex-row flex-wrap gap-2 p-1  rounded-sm max-h-28 overflow-y-auto items-center">
+                                    {task.assignees.map((value) => (
+                                        <UserCard
+                                            key={value._id}
+                                            firstName={value.firstName}
+                                            middleName={value.middleName}
+                                            lastName={value.lastName}
+                                            profileUrl={value.profilePicture}
+                                            onRemove={undefined}
+                                        />
+                                    ))}
+                                </div>
+                            </CardContent>
+                        </>
+                    ) : (
+                        <CardHeader>No assignees.</CardHeader>
+                    )}
                 </Card>
 
                 {/* due date */}
@@ -63,55 +73,64 @@ export function TaskView({ task }: { task: ITaskWithDetails }) {
                     <TaskDueDate
                         date={task.dueDate}
                         className="capitalize opacity-80"
-                    />
+                    />{" "}
+                    <Badge variant={task.workflowStage}>
+                        {task.workflowStage}
+                    </Badge>
                 </div>
 
                 {/* tags */}
-                {task.tags && (
-                    <Card className=" rounded-sm p-2 shadow-md">
-                        <CardHeader>
-                            <CardTitle className="flex flex-wrap gap-1">
-                                <div className="flex gap-1">
-                                    <Tag className="size-4" />
-                                    Tags
-                                </div>
-                                {task.tags.map((tag, index) => (
-                                    <Badge
-                                        key={index}
-                                        style={{
-                                            backgroundColor: tag.color,
-                                        }}
-                                    >
-                                        {tag.title}
-                                    </Badge>
-                                ))}
-                            </CardTitle>
-                        </CardHeader>{" "}
-                        {/* <CardContent>
-                        <div className=" flex flex-wrap gap-1">
-                            {task.tags.map((tag, index) => (
-                                <Badge
-                                    key={index}
-                                    style={{ backgroundColor: tag.color }}
-                                >
-                                    {tag.title}
-                                </Badge>
-                            ))}
-                        </div>
-                    </CardContent> */}
-                    </Card>
-                )}
+                <Card className=" rounded-sm">
+                    {
+                        task.tags.length > 0 ? (
+                            <CardHeader className="p-4">
+                                <CardTitle className="flex flex-wrap gap-1">
+                                    <div className="flex gap-1 mr-2">
+                                        <Tag className="size-4" />
+                                        Tags
+                                    </div>
+                                    {task.tags.map((tag, index) => (
+                                        <Badge
+                                            key={index}
+                                            style={{
+                                                backgroundColor: tag.color,
+                                            }}
+                                        >
+                                            {tag.title}
+                                        </Badge>
+                                    ))}
+                                </CardTitle>
+                            </CardHeader>
+                        ) : (
+                            <CardHeader>No tags.</CardHeader>
+                        )
+                        //      <CardContent>
+                        //     <div className=" flex flex-wrap gap-1">
+                        //         {task.tags.map((tag, index) => (
+                        //             <Badge
+                        //                 key={index}
+                        //                 style={{ backgroundColor: tag.color }}
+                        //             >
+                        //                 {tag.title}
+                        //             </Badge>
+                        //         ))}
+                        //     </div>
+                        // </CardContent>
+                    }
+                </Card>
 
-                {task.description && (
-                    <Card className="rounded-sm">
-                        <CardHeader>
+                <Card className="rounded-sm">
+                    {task.description ? (
+                        <CardHeader className="p-4">
                             <CardTitle>Description</CardTitle>
                             <CardDescription>
                                 {task.description}
                             </CardDescription>
                         </CardHeader>
-                    </Card>
-                )}
+                    ) : (
+                        <CardHeader>Description not provided.</CardHeader>
+                    )}
+                </Card>
 
                 <Button onClick={() => hideModal()}>Close</Button>
             </CardContent>
