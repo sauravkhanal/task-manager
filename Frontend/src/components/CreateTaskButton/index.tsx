5fb1927f2c0b8d829961b8d5c3569deb4b0a36ff
sauravkhanal <sauravkhanal635@gmail.com>
2024-06-07 12:10:00 +0545

    add mor props

--- Frontend/src/components/CreateTaskButton/index.tsx
+++ Frontend/src/components/CreateTaskButton/index.tsx
@@ -1,17 +1,41 @@
+import React from "react";
 import { useModal } from "@/context/modalContext";
 import CreateTask from "../CreateTask";
-import { Button } from "../ui/button";
+import { Button, ButtonProps } from "@/components/ui/button";
 import { ClipboardPlus } from "lucide-react";
 
-export default function CreateTaskButton() {
+interface CreateTaskButtonProps extends ButtonProps {
+    // variant?: ButtonProps["variant"];
+    // size?: ButtonProps["size"];
+    iconSize?: number;
+}
+
+const CreateTaskButton: React.FC<CreateTaskButtonProps> = ({
+    variant = "nav",
+    size,
+    children,
+    className,
+    iconSize = 5,
+    ...rest
+}) => {
     const { showModal } = useModal();
+
+    const handleClick = () => {
+        showModal(<CreateTask mode="create" />);
+    };
+
     return (
         <Button
-            variant="nav"
-            onClick={() => showModal(<CreateTask mode="create" />)}
+            variant={variant}
+            onClick={handleClick}
+            className={className}
+            {...rest}
+            size={size}
         >
-            <ClipboardPlus className="size-6 mr-2" />
-            Create Task
+            <ClipboardPlus className={`size-${iconSize} mr-2`} />
+            {children ? children : "Create Task"}
         </Button>
     );
-}
+};
+
+export default CreateTaskButton;
