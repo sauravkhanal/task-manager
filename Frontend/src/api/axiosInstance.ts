93430c892fd9600c21cd5a05e9891b70d42e7d32
sauravkhanal <sauravkhanal635@gmail.com>
2024-06-08 12:38:31 +0545

    add interceptor for no internet

--- Frontend/src/api/axiosInstance.ts
+++ Frontend/src/api/axiosInstance.ts
@@ -1,5 +1,6 @@
 import local from "@/utils/localStorage";
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
+import { toast } from "sonner";
 
 const axiosInstance: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_ENDPOINT as string,
@@ -20,6 +21,31 @@ axiosInstance.interceptors.request.use(
         return config;
     },
     (error: any) => {
+        return Promise.reject(error);
+    },
+);
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+    (response: AxiosResponse) => {
+        // If the response is successful, just return the response
+        return response;
+    },
+    (error: AxiosError) => {
+        console.log(error);
+        // Check if error is related to network/connectivity issues
+        if (!error.response) {
+            // Network error (e.g., no internet connection)
+            toast.error(
+                "Unable to connect to the internet. Please check your network connection and try again",
+            );
+            return Promise.resolve({
+                success: false,
+                message: "Internet disconnected",
+            });
+        }
+
+        // If error is not network-related, return the original error
         return Promise.reject(error);
     },
 );
