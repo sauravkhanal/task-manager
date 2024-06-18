781e2b2d165c5cd8b3f2b9d35be44235b2853710
sauravkhanal <sauravkhanal635@gmail.com>
2024-06-18 23:26:26 +0545

    fix: flag in docker run script

--- .github/workflows/scripts/dockerize-frontend.sh
+++ .github/workflows/scripts/dockerize-frontend.sh
@@ -5,4 +5,4 @@ cd /var/www/taskmanager-frontend
 docker build -t frontend:latest .
 docker stop frontend || true
 docker rm frontend || true
-docker run --restart unless-stopped --detach --bame frontend --publish 3000:80 frontend:latest
+docker run --restart unless-stopped --detach --name frontend --publish 3000:80 frontend:latest
