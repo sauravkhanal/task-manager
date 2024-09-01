72fea78e471ed1aef78675354180e8b20f094921
sauravkhanal <sauravkhanal635@gmail.com>
2024-09-01 15:29:57 +0545

    Why so serious?

--- .github/workflows/scripts/dockerize-backend.sh
+++ .github/workflows/scripts/dockerize-backend.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Building and running task-manager-backend Docker container..."
-cd /var/www/taskmanager-task-manager-backend
-docker build -t task-manager-backend:latest .
-docker stop task-manager-backend || true
-docker rm task-manager-backend || true
+echo "Building and running taskmanager-backend Docker container..."
+cd /var/www/taskmanager-backend
+docker build -t taskmanager-backend:latest .
+docker stop taskmanager-backend || true
+docker rm taskmanager-backend || true
 
-docker run -d --restart unless-stopped --no-cahce --name task-manager-backend -p 8000:8000 \
+docker run -d --restart unless-stopped --no-cahce --name taskmanager-backend -p 8000:8000 \
   -e PORT="${PORT}" \
   -e CORS="${CORS}" \
   -e ENDPOINT="${ENDPOINT}" \
@@ -15,4 +15,4 @@ docker run -d --restart unless-stopped --no-cahce --name task-manager-backend -p
   -e EMAIL_USERNAME="${EMAIL_USERNAME}" \
   -e EMAIL_PASSWORD="${EMAIL_PASSWORD}" \
   -e CORS_ORIGINS="${CORS_ORIGINS}" \
-  task-manager-backend:latest
+  taskmanager-backend:latest
--- .github/workflows/scripts/dockerize-frontend.sh
+++ .github/workflows/scripts/dockerize-frontend.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Building and running task-manager-frontend Docker container..."
-cd /var/www/taskmanager-task-manager-frontend
-docker build --no-cache -t task-manager-frontend:latest .
-docker stop task-manager-frontend || true
-docker rm task-manager-frontend || true
-docker run --restart unless-stopped --detach --name task-manager-frontend --publish 3000:80 task-manager-frontend:latest
+echo "Building and running taskmanager-frontend Docker container..."
+cd /var/www/taskmanager-frontend
+docker build --no-cache -t taskmanager-frontend:latest .
+docker stop taskmanager-frontend || true
+docker rm taskmanager-frontend || true
+docker run --restart unless-stopped --detach --name taskmanager-frontend --publish 3000:80 taskmanager-frontend:latest
